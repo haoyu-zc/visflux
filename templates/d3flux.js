8da6aed93ecefa3821477f4a87f2e908ea2e553c
Hari Zhang <hari.k.zhang@gmail.com>
2019-05-05 11:21:42 +0800

    feat: svg zoom
    potential bug: svg dragging

--- templates/d3flux.js
+++ templates/d3flux.js
@@ -77,6 +77,63 @@ require(["cola", "d3", "math", "FileSaver", ], function (cola, d3, math, FileSav
     // Append the CSS styles
     svg.append("style").text('{{ css }}');
 
+    // Zoom function
+    
+    var zoom = d3.behavior.zoom().scaleExtent([0.5, 8]).on("zoom", zoomed);
+    function zoomed() {
+      svg.attr("transform",
+          "translate(" + zoom.translate() + ")" +
+          "scale(" + zoom.scale() + ")"
+      );
+    }
+  
+    function interpolateZoom (translate, scale) {
+        var self = this;
+        return d3.transition().duration(350).tween("zoom", function () {
+            var iTranslate = d3.interpolate(zoom.translate(), translate),
+                iScale = d3.interpolate(zoom.scale(), scale);
+            return function (t) {
+                zoom
+                    .scale(iScale(t))
+                    .translate(iTranslate(t));
+                zoomed();
+            };
+        });
+    }
+
+    function zoomClick() {
+      var clicked = d3.event.target,
+          direction = 1,
+          factor = 0.2,
+          target_zoom = 1,
+          center = [width / 2, height / 2],
+          extent = zoom.scaleExtent(),
+          translate = zoom.translate(),
+          translate0 = [],
+          l = [],
+          view = {x: translate[0], y: translate[1], k: zoom.scale()};
+  
+      d3.event.preventDefault();
+      direction = (this.id === 'zoom_in') ? 1 : -1;
+      target_zoom = zoom.scale() * (1 + factor * direction);
+  
+      if (target_zoom < extent[0] || target_zoom > extent[1]) { return false; }
+  
+      translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
+      view.k = target_zoom;
+      l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
+  
+      view.x += center[0] - l[0];
+      view.y += center[1] - l[1];
+  
+      interpolateZoom([view.x, view.y], view.k);
+  }
+  
+    d3.selectAll('.zoom_button').on('click', zoomClick);
+
+    svg.call(zoom);
+
+
     // Code for the figure manipulation buttons.
     d3.select("#{{ figure_id }}_options .reactionbutton").on("click", function() {
       // Show/hide the reaction control node points.
