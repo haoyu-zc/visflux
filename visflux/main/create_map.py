5af39c35bbf747472725b075080899de84830295
ikspike <clifford.k.zhang@outlook.com>
2020-12-26 19:38:06 +0800

    Update create_map.py

--- visflux/main/create_map.py
+++ visflux/main/create_map.py
@@ -2,11 +2,9 @@ from cobra.flux_analysis import pfba
 import itertools
 import cobra
 import sys
-import json
-import os
 
 
-import visflux
+import visflux as d3f
 from visflux.core.flux_layouts import render_model
 from visflux import flux_map
 
@@ -65,27 +63,7 @@ for obj in itertools.chain(model.reactions, model.metabolites):
 pfba(model)
 
 
-# Merge JS dependencies into d3flux.js
 
-# Load the enviroment of visflux
-template_path = os.path.dirname(visflux.__file__) + os.path.sep + 'templates'
-
-with open(template_path + os.path.sep + 'include.json') as f:
-    data = json.load(f)
-
-
-print(data['local'])
-
-include_files = data['local']
-
-merged_js = open(template_path + os.path.sep + 'd3flux.js', 'r+')
-# Clear exsisting content
-merged_js.truncate(0)
-
-merged_js = open(template_path + os.path.sep +'d3flux.js', mode='w', encoding='utf-8')
-for include_file, path in include_files.items():
-    temp = open(path, mode='r', encoding='utf-8')
-    merged_js.write(temp.read())
 
 
 # Output html file
